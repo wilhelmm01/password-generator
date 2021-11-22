8c209f99715cb0eda5136f2c607e3a9029842a64
Your Name <matt14w@gmail.com>
2021-11-21 19:03:14 -0500

    final

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
 // Assignment code here
+var passLength = window.prompt ("Desired Password Length?");
+
+var charset = {
+  upperCase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+  lowerCase: "abcdefghijklmnopqrstuvwxyz",
+  numbers: "1234567890",
+  specChar: "!@#$%^&*()",
+}
+
+
+function generatePassword () {
+   length = passLength,
+   charset = "!@#$%^&*()1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+   random = "";
+
+   for (var i = 0; i < length; i++) {
+    random += charset.charAt(Math.floor(Math.random()* charset.length));
+   }
+return random;
+
+}
 
 
 // Get references to the #generate element
@@ -8,10 +29,17 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+   
 
   passwordText.value = password;
 
 }
 
-// Add event listener to generate button
+
+
+ 
+
 generateBtn.addEventListener("click", writePassword);
+
+
+  
